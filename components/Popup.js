9c8582691be8a285267c8a6f8845309138d6f3a2
Robert Anisimov <skillup23@yandex.ru>
2023-07-24 17:21:29 +0300

    Новость 24.07 еще 2 Правки

--- components/Popup.js
+++ components/Popup.js
@@ -5,13 +5,13 @@ function Popup({ children, isOpen, setOpen }) {
   return (
     <div
       onClick={(e) => e.currentTarget === e.target && setOpen(false)}
-      className={`fixed w-full h-full top-0 left-0 bg-black/[.9] flex items-center justify-center z-[55] ${
+      className={`fixed w-full min-h-full top-0 left-0 bg-black/[.9] flex items-center justify-center z-[55] ${
         isOpen ? 'block overflow-auto' : 'hidden'
       } `}
     >
       <div className="relative">
         <VscClose
-          className="absolute top-5 sm:top-2 right-1 sm:right-2 text-3xl cursor-pointer"
+          className="absolute top-1 sm:top-2 right-1 sm:right-2 text-3xl cursor-pointer"
           onClick={() => setOpen(false)}
         />
         {children}
