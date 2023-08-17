58c8193440ee92058b6d3b20302ee20e67830f58
Robert Anisimov <skillup23@yandex.ru>
2023-08-17 14:21:30 +0300

    Правки от заказчика 0.65

--- components/ImageLink.js
+++ components/ImageLink.js
@@ -3,11 +3,11 @@ import React from 'react';
 import Image from 'next/image';
 
 function ImageLink({ href, src, alt, width, cssClass }) {
-  const widthImg = !width ? `w-36 sm:w-48 md:w-56` : `w-${width}`;
+  const widthImg = !width ? `m-auto w-36 sm:w-48 md:w-56` : `m-auto w-${width}`;
   // const widthImg = width ? `w-${width}` : 'w-36 sm:w-56';
   const otherClass = cssClass
-    ? `ease-in duration-200 hover:scale-105 ${cssClass}`
-    : `ease-in duration-200 hover:scale-105`;
+    ? `m-auto ease-in duration-200 hover:scale-105 ${cssClass}`
+    : `m-auto ease-in duration-200 hover:scale-105`;
 
   return (
     <Link href={href} className={widthImg}>
