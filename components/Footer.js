cc4eb047e655cbe0eb7b17fa54fa5f4d0adbf3c6
Robert Anisimov <skillup23@yandex.ru>
2024-06-25 12:54:35 +0300

    Юридическая информация 0.1

--- components/Footer.js
+++ components/Footer.js
@@ -9,7 +9,7 @@ import { SlSocialVkontakte } from 'react-icons/sl';
 function Footer() {
   return (
     <footer className="shrink-0 bg-white py-5 mt-16">
-      <nav className="lg:container mx-auto flex justify-between items-center px-2 lg:pr-5">
+      <nav className="lg:container mx-auto flex justify-between items-center min-[320px]:px-2 min-[400px]:px-6 sm:px-2 lg:pr-5">
         <Link
           href="/"
           className="w-[75px] h-[35px] lg:w-[100px] lg:h-[51px] xl:w-[125px] xl:h-[62px] relative"
@@ -24,8 +24,8 @@ function Footer() {
           />
         </Link>
 
-        <div className="grid grid-cols-4 md:flex items-center justify-between gap-2 sm:gap-4 md:gap-4 lg:gap-6 xl:gap-8 2xl:gap-12">
-          {menu.slice(0, 8).map(({ id, name, link }) => {
+        <div className="grid grid-cols-3 sm:grid-cols-4 md:flex items-center justify-between gap-x-8 gap-y-4 sm:gap-4 md:gap-4 lg:gap-6 xl:gap-8 2xl:gap-12">
+          {menu.slice(0, 4).map(({ id, name, link }) => {
             return (
               <Link
                 key={id}
@@ -36,13 +36,35 @@ function Footer() {
               </Link>
             );
           })}
+          {menu.slice(6, 8).map(({ id, name, link }) => {
+            return (
+              <Link
+                key={id}
+                href={link}
+                className="text-lg sm:text-xl xl:text-2xl ease-in duration-200 hover:scale-105 text-black text-center"
+              >
+                {name}
+              </Link>
+            );
+          })}
+          {menu.slice(10, 11).map(({ id, name, link }) => {
+            return (
+              <Link
+                key={id}
+                href={link}
+                className="text-lg sm:text-xl xl:text-2xl ease-in duration-200 hover:scale-105 text-black text-center col-span-3 sm:col-span-2"
+              >
+                {name}
+              </Link>
+            );
+          })}
         </div>
         <div className="hidden sm:flex ml-6">
           <Link href="https://vk.com/trkcitycentr" target={'_blank'}>
-            <SlSocialVkontakte className="text-xl sm:text-2xl xl:text-4xl ease-in duration-200 hover:scale-105 text-black mr-5" />
+            <SlSocialVkontakte className="text-xl sm:text-3xl xl:text-4xl ease-in duration-200 hover:scale-105 text-black mr-5" />
           </Link>
           <Link href="https://t.me/ciiitycenter" target={'_blank'}>
-            <FaTelegramPlane className="text-xl sm:text-4xl xl:text-4xl ease-in duration-200 hover:scale-105 text-black" />
+            <FaTelegramPlane className="text-xl sm:text-3xl xl:text-4xl ease-in duration-200 hover:scale-105 text-black" />
           </Link>
         </div>
       </nav>
---
+++ pages/documents/index.js
@@ -0,0 +1,120 @@
+import DocumentPdf from '@/components/DocumentPdf';
+import { useState } from 'react';
+
+const { default: Layout } = require('@/components/Layout');
+
+function Documents() {
+  const [isGeneralDocs, setGeneralDocs] = useState(true);
+  const [isPersonalData, setPersonalData] = useState(false);
+  const [isArchDocs, setArchDocs] = useState(false);
+
+  function activeGeneralDocs() {
+    setGeneralDocs(true);
+    setPersonalData(false);
+    setArchDocs(false);
+  }
+
+  function activePersonalData() {
+    setGeneralDocs(false);
+    setPersonalData(true);
+    setArchDocs(false);
+  }
+
+  function activeArchDocs() {
+    setGeneralDocs(false);
+    setPersonalData(false);
+    setArchDocs(true);
+  }
+
+  return (
+    <Layout title="Юридическая информация">
+      <section className="mt-10">
+        <div className="lg:container mx-auto px-4 lg:px-0">
+          <h1 className="title_main mt-10">Юридическая информация</h1>
+          <ul className="mt-6 lg:mt-14 flex flex-wrap w-full items-center justify-between">
+            <div
+              onClick={activeGeneralDocs}
+              className={`link_kino w-full sm:w-1/3 text-base sm:text-xl md:text-2xl cursor-pointer ${
+                isGeneralDocs ? 'bg-black text-white' : ''
+              }`}
+            >
+              Общие документы
+            </div>
+            <div
+              onClick={activePersonalData}
+              className={`link_kino w-full sm:w-1/3 text-base sm:text-xl md:text-2xl cursor-pointer ${
+                isPersonalData ? 'bg-black text-white' : ''
+              }`}
+            >
+              Персональные данные
+            </div>
+            <div
+              onClick={activeArchDocs}
+              className={`link_kino w-full sm:w-1/3 text-base sm:text-xl md:text-2xl cursor-pointer ${
+                isArchDocs ? 'bg-black text-white' : ''
+              }`}
+            >
+              Архив документов
+            </div>
+          </ul>
+
+          <div
+            className={`${
+              isGeneralDocs ? '' : 'hidden'
+            } mt-14 flex flex-col gap-4 md:gap-8`}
+          >
+            <DocumentPdf
+              href={
+                '/docs/Правила для Арендаторов ТРК «Сити Центр». Версия от 17.06.2024.pdf'
+              }
+              title={'Правила для Арендаторов ТРК «Сити Центр»'}
+              date={'Версия от 17.06.2024'}
+            />
+            <DocumentPdf
+              href={
+                '/docs/Регламент Работ для Арендаторов ТРК «Сити Центр». Версия от 27.05.2024.pdf'
+              }
+              title={'Регламент Работ для Арендаторов ТРК «Сити Центр»'}
+              date={'Версия от 27.05.2024'}
+            />
+          </div>
+
+          <div
+            className={`${
+              isPersonalData ? '' : 'hidden'
+            } mt-14 flex flex-col gap-4 md:gap-8`}
+          >
+            <DocumentPdf
+              href={'#'}
+              title={`Политика обработки персональных данных в\u00A0ТРК\u00A0«Сити\u00A0Центр»`}
+              date={'Версия от 17.06.2024'}
+            />
+          </div>
+
+          <div
+            className={`${
+              isArchDocs ? '' : 'hidden'
+            } mt-14 flex flex-col gap-4 md:gap-8`}
+          >
+            <DocumentPdf
+              href={
+                '/docs/arch/Правила для Арендаторов ТРК «Сити Центр». Версия от 28.05.2024.pdf'
+              }
+              title={'Правила для Арендаторов ТРК «Сити Центр»'}
+              date={'Версия от 28.05.2024'}
+            />
+            <DocumentPdf
+              href={
+                '/docs/arch/Регламент Работ для Арендаторов ТРК «Сити Центр». Версия от 09.04.2024.pdf'
+              }
+              title={'Регламент Работ для Арендаторов ТРК «Сити Центр»'}
+              date={'Версия от 09.04.2024'}
+            />
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+export default Documents;
