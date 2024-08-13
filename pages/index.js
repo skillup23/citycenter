bfe731fc9ea6f7079575b78ff31477a7844324b6
Robert Anisimov <skillup23@yandex.ru>
2024-08-13 14:18:28 +0300

    Мероприятия 130824

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,8 @@ import event3 from '@/public/site_img/event3.png';
 import event4 from '@/public/site_img/event4.png';
 import news7 from '@/public/news/7.jpg';
 import news1 from '@/public/news/1.jpg';
-import news1_1 from '@/public/news/1-1.jpg';
 import news8 from '@/public/news/8.jpg';
+import news9 from '@/public/news/9.jpg';
 import PrezentMain from '@/public/site_img/PrezentMain.jpg';
 import logoMonitor1 from '@/public/site_img/лого-монитор.png';
 import logoMonitor2 from '@/public/site_img/лого-монитор-2.png';
@@ -170,6 +170,20 @@ export default function Home() {
               className="block relative w-full overflow-hidden h-full"
             >
               <Image
+                src={news9}
+                alt="Новость"
+                width="0"
+                height="0"
+                style={{ width: '100%', height: '100%' }}
+                priority
+              />
+              <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/9.jpg")]'></div>
+            </Link>
+            <Link
+              href="/news"
+              className="block relative w-full overflow-hidden h-full"
+            >
+              <Image
                 src={news8}
                 alt="Новость"
                 width="0"
@@ -206,20 +220,6 @@ export default function Home() {
                 priority
               />
               <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/1.jpg")]'></div>
-            </Link>
-            <Link
-              href="/news"
-              className="block relative w-full overflow-hidden h-full"
-            >
-              <Image
-                src={news1_1}
-                alt="Новость"
-                width="0"
-                height="0"
-                style={{ width: '100%', height: '100%' }}
-                priority
-              />
-              <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/1-1.jpg")]'></div>
             </Link>
           </div>
         </div>
