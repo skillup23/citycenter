4d91e2d5956152ecbd5c81227e4407ae1223ab93
skillup23 <skillup23@yandex.ru>
2025-02-16 13:29:49 +0300

    правки 160225

--- components/Footer.js
+++ components/Footer.js
@@ -1,14 +1,13 @@
-import Link from 'next/link';
-import React from 'react';
-import Image from 'next/image';
-import logoBlack from '@/public/site_img/logoBlack.png';
-import { menu } from '@/public/data/data';
-import { FaTelegramPlane } from 'react-icons/fa';
-import { SlSocialVkontakte } from 'react-icons/sl';
+import { menu } from "@/public/data/data";
+import logoBlack from "@/public/site_img/logoBlack.png";
+import Image from "next/image";
+import Link from "next/link";
+import { FaTelegramPlane } from "react-icons/fa";
+import { SlSocialVkontakte } from "react-icons/sl";
 
 function Footer() {
   return (
-    <footer className="shrink-0 bg-white py-5 mt-16">
+    <footer className="shrink-0 bg-white py-5 sm:mt-16">
       <nav className="lg:container mx-auto flex justify-between items-center min-[320px]:px-3 min-[400px]:px-6 sm:px-2 lg:pr-5">
         <Link
           href="/"
@@ -61,10 +60,10 @@ function Footer() {
           })}
         </div>
         <div className="hidden sm:flex ml-6">
-          <Link href="https://vk.com/trkcitycentr" target={'_blank'}>
+          <Link href="https://vk.com/trkcitycentr" target={"_blank"}>
             <SlSocialVkontakte className="text-xl sm:text-3xl xl:text-4xl ease-in duration-200 hover:scale-105 text-black mr-5" />
           </Link>
-          <Link href="https://t.me/ciiitycenter" target={'_blank'}>
+          <Link href="https://t.me/ciiitycenter" target={"_blank"}>
             <FaTelegramPlane className="text-xl sm:text-3xl xl:text-4xl ease-in duration-200 hover:scale-105 text-black" />
           </Link>
         </div>
