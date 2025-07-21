86fa3c201ef7e0b6943de940f7f7b0fb5079e2fc
Robert Anisimov <skillup23@yandex.ru>
2025-07-21 10:48:58 +0300

    бутики 210725

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '@/components/Layout';
 import ShopListMain from '@/components/ShopListMain';
 import SliderMain from '@/components/SliderMain';
 import news42 from '@/public/news/42.jpg';
-import news37 from '@/public/news/37.jpg';
+import news43 from '@/public/news/43.jpg';
 import news40 from '@/public/news/40.jpg';
 import news41 from '@/public/news/41.jpg';
 import kinoBanner from '@/public/slide_main/kino-banner-12.jpg';
@@ -125,6 +125,20 @@ export default function Home() {
               className="block relative w-full overflow-hidden h-full"
             >
               <Image
+                src={news43}
+                alt="Новость"
+                width="0"
+                height="0"
+                style={{ width: '100%', height: '100%' }}
+                priority
+              />
+              <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/43.jpg")]'></div>
+            </Link>
+            <Link
+              href="/news#new2"
+              className="block relative w-full overflow-hidden h-full"
+            >
+              <Image
                 src={news42}
                 alt="Новость"
                 width="0"
@@ -135,7 +149,7 @@ export default function Home() {
               <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/42.jpg")]'></div>
             </Link>
             <Link
-              href="/news#new2"
+              href="/news#new3"
               className="block relative w-full overflow-hidden h-full"
             >
               <Image
@@ -149,7 +163,7 @@ export default function Home() {
               <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/41.jpg")]'></div>
             </Link>
             <Link
-              href="/news#new3"
+              href="/news#new4"
               className="block relative w-full overflow-hidden h-full"
             >
               <Image
@@ -161,20 +175,6 @@ export default function Home() {
                 priority
               />
               <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/40.jpg")]'></div>
-            </Link>
-            <Link
-              href="/news"
-              className="block relative w-full overflow-hidden h-full"
-            >
-              <Image
-                src={news37}
-                alt="Новость"
-                width="0"
-                height="0"
-                style={{ width: '100%', height: '100%' }}
-                priority
-              />
-              <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/37.jpg")]'></div>
             </Link>
           </div>
         </div>
