de66bbc2e9ca7c1f9bffc69dee7413b65431414e
Robert Anisimov <skillup23@yandex.ru>
2025-07-29 08:59:29 +0300

    rent 290725

--- pages/rent/index.js
+++ pages/rent/index.js
@@ -1,20 +1,20 @@
-import Layout from "@/components/Layout";
-import Popup from "@/components/Popup";
-import noimg from "@/public/butiks/loaderImg.png";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
-import { BsFilePdf } from "react-icons/bs";
-import { FaPhoneAlt } from "react-icons/fa";
-import { HiLocationMarker } from "react-icons/hi";
-import { MdEmail } from "react-icons/md";
+import Layout from '@/components/Layout';
+import Popup from '@/components/Popup';
+import noimg from '@/public/butiks/loaderImg.png';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useState } from 'react';
+import { BsFilePdf } from 'react-icons/bs';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { HiLocationMarker } from 'react-icons/hi';
+import { MdEmail } from 'react-icons/md';
 
 function Rent() {
   const [isOpen, setOpen] = useState(false);
   const [isModal, setIsModal] = useState({
     id: 1,
     src: noimg,
-    alt: "Описание",
+    alt: 'Описание',
   });
 
   function test(id, src, alt) {
@@ -25,18 +25,18 @@ function Rent() {
   const foto = [
     {
       id: 1,
-      src: "/rent_img/1-4-floor.jpg",
-      alt: "1 этаж",
+      src: '/rent_img/1-4-floor.jpg',
+      alt: '1 этаж',
     },
     {
       id: 2,
-      src: "/rent_img/2-4-floor.jpg",
-      alt: "2 этаж",
+      src: '/rent_img/2-5-floor.jpg',
+      alt: '2 этаж',
     },
     {
       id: 3,
-      src: "/rent_img/3-4-floor.jpg",
-      alt: "3 этаж",
+      src: '/rent_img/3-5-floor.jpg',
+      alt: '3 этаж',
     },
   ];
 
