67da34450ddea1793fd84eb28186a756752f878e
skillup23 <skillup23@yandex.ru>
2025-12-10 20:27:02 +0300

    кино 101225

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,25 @@
 // import ImageLink from '@/components/ImageLink';
-import DocumentsPage from '@/components/DocumentsPage';
-import Layout from '@/components/Layout';
-import MarqueeLogos from '@/components/MarqueeLogos';
-import ShopListMain from '@/components/ShopListMain';
-import SliderMain from '@/components/SliderMain';
-import VideoMain from '@/components/VideoMain';
-import YandexMap from '@/components/YandexMap';
-import { kinoAfisha, shopList, sliderTop } from '@/public/data/data';
-import news66 from '@/public/news/66.jpg';
-import news67 from '@/public/news/67.jpg';
-import news68 from '@/public/news/68.jpg';
-import news69 from '@/public/news/69.jpg';
-import imgShowroom from '@/public/showroom_img/showroomMainResize.png';
-import imgShowroomMob from '@/public/showroom_img/showroomMainResizeMob.png';
-import kinoBanner from '@/public/slide_main/kino-banner-12.jpg';
-import Image from 'next/image';
-import Link from 'next/link';
-import { FaBus, FaPhoneAlt } from 'react-icons/fa';
-import { HiLocationMarker } from 'react-icons/hi';
-import { MdEmail } from 'react-icons/md';
-import { RiTimeFill } from 'react-icons/ri';
+import DocumentsPage from "@/components/DocumentsPage";
+import Layout from "@/components/Layout";
+// import MarqueeLogos from '@/components/MarqueeLogos';
+import ShopListMain from "@/components/ShopListMain";
+import SliderMain from "@/components/SliderMain";
+import VideoMain from "@/components/VideoMain";
+import YandexMap from "@/components/YandexMap";
+import { kinoAfisha, shopList, sliderTop } from "@/public/data/data";
+import news66 from "@/public/news/66.jpg";
+import news67 from "@/public/news/67.jpg";
+import news68 from "@/public/news/68.jpg";
+import news69 from "@/public/news/69.jpg";
+import imgShowroom from "@/public/showroom_img/showroomMainResize.png";
+import imgShowroomMob from "@/public/showroom_img/showroomMainResizeMob.png";
+import kinoBanner from "@/public/slide_main/kino-banner-12.jpg";
+import Image from "next/image";
+import Link from "next/link";
+import { FaBus, FaPhoneAlt } from "react-icons/fa";
+import { HiLocationMarker } from "react-icons/hi";
+import { MdEmail } from "react-icons/md";
+import { RiTimeFill } from "react-icons/ri";
 
 export default function Home() {
   return (
@@ -54,7 +54,7 @@ export default function Home() {
                 alt="Новость"
                 width="0"
                 height="0"
-                style={{ width: '100%', height: '100%' }}
+                style={{ width: "100%", height: "100%" }}
                 priority
               />
               <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/69.jpg")]'></div>
@@ -68,7 +68,7 @@ export default function Home() {
                 alt="Новость"
                 width="0"
                 height="0"
-                style={{ width: '100%', height: '100%' }}
+                style={{ width: "100%", height: "100%" }}
                 priority
               />
               <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/68.jpg")]'></div>
@@ -82,7 +82,7 @@ export default function Home() {
                 alt="Новость"
                 width="0"
                 height="0"
-                style={{ width: '100%', height: '100%' }}
+                style={{ width: "100%", height: "100%" }}
                 priority
               />
               <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/67.jpg")]'></div>
@@ -96,7 +96,7 @@ export default function Home() {
                 alt="Новость"
                 width="0"
                 height="0"
-                style={{ width: '100%', height: '100%' }}
+                style={{ width: "100%", height: "100%" }}
                 priority
               />
               <div className='background_scale duration-300 text-center text-2xl md:text-3xl sm:text-4xl p-6 bg-[url("../public/news/66.jpg")]'></div>
@@ -118,15 +118,15 @@ export default function Home() {
                 <Link
                   key={id}
                   href="https://kinomonitor.ru/cinemas/111/seances"
-                  className="block relative w-full overflow-hidden h-full"
-                  target={'_blank'}
+                  className="block relative w-full overflow-hidden aspect-[3/5]"
+                  target={"_blank"}
                 >
                   <Image
                     src={img}
                     alt="Киноафиша"
-                    width="0"
-                    height="0"
-                    className="ease-in duration-150 md:grayscale hover:grayscale-0"
+                    width={0}
+                    height={0}
+                    className="w-full h-full ease-in duration-150 md:grayscale hover:grayscale-0"
                     priority
                   />
                 </Link>
@@ -138,12 +138,12 @@ export default function Home() {
 
       {/*                      СЛАЙДЕР КИНОТЕАТР                         */}
 
-      <section className="relative mt-2 sm:mt-20" id="kinoteatr">
+      <section className="relative mt-10 sm:mt-20" id="kinoteatr">
         {/* <SliderMain data={sliderCenter} dots={true} /> */}
         <div className="lg:container mx-auto">
           <Link
             href="https://wa.me/79994120012?text=Добрый%20день.%20Расскажите,%20пожалуйста,%20об%20условиях%20аренды%20кинозала"
-            target={'_blank'}
+            target={"_blank"}
           >
             <Image
               src={kinoBanner}
@@ -214,7 +214,7 @@ export default function Home() {
             </div>
 
             <div className="w-full sm:w-[45%]">
-              <VideoMain videoLink={'/site_img/city_FHD_6.mp4'} />
+              <VideoMain videoLink={"/site_img/city_FHD_6.mp4"} />
             </div>
           </div>
         </div>
